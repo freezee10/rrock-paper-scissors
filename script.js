cb55ae3509e8ad5f97ee3fbf207b2c72da49b38c
Addison Young <addisonyoung0902@gmail.com>
2025-04-01 15:15:04 -0400

    configure buttons

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 
+let rockButton = document.querySelector(".rock");
+let paperButton = document.querySelector(".paper");
+let scissorsButton = document.querySelector(".scissors");
+
+rockButton.addEventListener('click', () => {
+  playRound("Rock", getComputerChoice());
+});
+
+paperButton.addEventListener('click', () => {
+  playRound("Paper", getComputerChoice());
+});
+scissorsButton.addEventListener('click', () => {
+  playRound("Scissors", getComputerChoice());
+});
+
 function getComputerChoice()
 {
   let randomNumber = Math.floor((Math.random() * 3));
@@ -18,83 +33,65 @@ function getComputerChoice()
 
 }
 
-function getHumanChoice()
+let humanScore = 0;
+let computerScore = 0;
+
+function checkVictory()
 {
-  // case sensitive
-  let humanChoice = prompt("Rock, paper, or scissors? ");
-  return humanChoice;
+  if(humanScore == 5)
+  {
+    // something
+  }
+  else if(computerScore === 5)
+  {
+    // something
+  }
 }
 
-
-
-function playGame()
+function playRound(humanChoice, computerChoice)
 {
-  let humanScore = 0;
-  let computerScore = 0;
-  function playRound(humanChoice, computerChoice)
+humanChoice = humanChoice.toUpperCase();
+computerChoice = computerChoice.toUpperCase();
+if(humanChoice === computerChoice)
 {
-  humanChoice = humanChoice.toUpperCase();
-  computerChoice = computerChoice.toUpperCase();
-  if(humanChoice === computerChoice)
-  {
-    console.log("Tie! Both played " + humanChoice);
-    // tie case
-  }
-  else if(humanChoice === "ROCK" && computerChoice === "PAPER")
-  {
-    computerScore++;
-    console.log("Computer wins! Computer played: " + computerChoice + " human played: " + humanChoice);
+  console.log("Tie! Both played " + humanChoice);
+  // tie case
+}
+else if(humanChoice === "ROCK" && computerChoice === "PAPER")
+{
+  computerScore++;
+  console.log("Computer wins! Computer played: " + computerChoice + " human played: " + humanChoice);
+  // case
+}
+else if(humanChoice === "ROCK" && computerChoice === "SCISSORS")
+{
+  humanScore++;
+  console.log("Human wins! Computer played: " + computerChoice + " human played: " + humanChoice);
     // case
-  }
-  else if(humanChoice === "ROCK" && computerChoice === "SCISSORS")
-  {
-    humanScore++;
-    console.log("Human wins! Computer played: " + computerChoice + " human played: " + humanChoice);
-      // case
-  }
-  else if(humanChoice === "PAPER" && computerChoice === "ROCK")
-  {
-    humanScore++;
-    console.log("Human wins! Computer played: " + computerChoice + " human played: " + humanChoice);
-      // case
-  }
-  else if(humanChoice === "PAPER" && computerChoice === "SCISSORS")
-  {
-    computerScore++;
-    console.log("Computer wins! Computer played: " + computerChoice + " human played: " + humanChoice);
-      // case
-  }
-  else if(humanChoice === "SCISSORS" && computerChoice === "PAPER")
-  {
-    humanScore++;
-    console.log("Human wins! Computer played: " + computerChoice + " human played: " + humanChoice);
-      // case
-  }
-  else if(humanChoice === "SCISSORS" && computerChoice === "ROCK")
-  {
-    computerScore++;
-    console.log("Computer wins! Computer played: " + computerChoice + " human played: " + humanChoice);
-      // case
-   }
+}
+else if(humanChoice === "PAPER" && computerChoice === "ROCK")
+{
+  humanScore++;
+  console.log("Human wins! Computer played: " + computerChoice + " human played: " + humanChoice);
+    // case
+}
+else if(humanChoice === "PAPER" && computerChoice === "SCISSORS")
+{
+  computerScore++;
+  console.log("Computer wins! Computer played: " + computerChoice + " human played: " + humanChoice);
+    // case
+}
+else if(humanChoice === "SCISSORS" && computerChoice === "PAPER")
+{
+  humanScore++;
+  console.log("Human wins! Computer played: " + computerChoice + " human played: " + humanChoice);
+    // case
+}
+else if(humanChoice === "SCISSORS" && computerChoice === "ROCK")
+{
+  computerScore++;
+  console.log("Computer wins! Computer played: " + computerChoice + " human played: " + humanChoice);
+    // case
+ }
 }
 
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  if(computerScore > humanScore)
-  {
-    console.log("Computer wins with " + computerScore + " vs human score: " + humanScore);
-  }
-  else if(humanScore > computerScore)
-  {
-    console.log("Human wins with " + humanScore + " vs computer score: " + computerScore);
-  }
-  else
-  {
-    console.log("TIE! Both had a score of " + humanScore);
-  }
-}
-
-playGame();
